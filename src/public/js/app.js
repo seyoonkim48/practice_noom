dee8cc4b456e8711a8a9da622791780230be573e
seyoonkim48 <seyoonkim48@gmail.com>
2021-09-24 18:18:05 +0900

    Feat: Chat using Websocket
    웹소켓을 이용한 실시간 채팅(익명, 닉네임 설정 가능)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,55 @@
-alert("hi!")
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+// nickname과 payload(message)가 둘 다 웹소켓으로 전달되어야하는 내용이라 구분이 필요함.
+// JSON형태로 주고받는 방법을 생각해보자! 하지만 Text로만 전송이 가능하기 때문에 별도의 stringify, parse 과정이 필요하다.
+function makeMessage(type, payload) {
+  const msg = {type, payload};
+  return JSON.stringify(msg);
+}
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  // console.log("New message: ", message.data);
+  // li태그에 메세지 넣고 추가
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected to Server ❌");
+});
+
+// setTimeout(() => {
+//   socket.send("hello from the Browser!");
+// }, 10000);
+
+function handleSubmit (event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  // socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
+
+  const li = document.createElement("li");
+  li.innerText = `You: ${input.value}`;
+  messageList.append(li);
+
+  input.value = "";
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import Websocket from "ws";
 import express from "express";
 
 const app = express();
@@ -8,5 +10,51 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+// only http
+// const handleListen = () => console.log(`Listening on http://localhost:3000`);
+// app.listen(3000, handleListen);
+
+// + Websocket
+// http, ws가 함께 3000번 포트에서 열리길 원해서 이렇게 작성함!
+// express로 http서버를 만들었고
+// http서버 위에 Web Socket Server를 만들었음
+// ==> 3000번 포트에서 http와 ws의 req, res를 모두 처리
+const handleListen = () => console.log(`Listening on http(|| ws)://localhost:3000`);
+const server = http.createServer(app);
+const wss = new Websocket.Server({ server });
+
+// fake db
+const sockets = []; // 연결이 되면 어디와 연결된 건지를 여기에 저장함. ex)sockets = [chrome, safari];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anonymous";
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => {
+    console.log("Disconnected to Browser ❌");
+  });
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    // console.log(message, msg.toString('utf-8'));
+    switch(message.type) {
+      case "new_message":
+        // sockets.forEach((aSocket) => aSocket.send(message.payload));
+        sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+        break;
+      case "nickname":
+        // console.log(message.payload);
+        socket["nickname"] = message.payload;
+        break;
+    }
+
+    // aSocket =>  each browser
+    // console.log(msg.toString('utf-8'));
+    // socket.send(msg.toString('utf-8')); //-> 이렇게하면 여러 브라우저가 있을 때, 각각 서버와 핑퐁할 뿐 서로의 메세지를 볼 수 없음
+    // sockets.forEach((aSocket) => {
+    //   aSocket.send(msg.toString('utf-8'));
+    // });
+  });
+  // socket.send("hello!!!!");
+});
+
+server.listen(3000, handleListen);
